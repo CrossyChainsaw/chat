83f9e4b35324504a72184b837b187ae2f4bacd0b
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2025-01-24 00:32:59 +0100

    refactor

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from flask_socketio import SocketIO, send, join_room
 import random
 app = Flask(__name__)
 socketio = SocketIO(app)
-app.secret_key = 'your-secret-key'  # Important for session handling
+app.secret_key = 'your-secret-key'
 
 # Global variables to store connected users and messages
 users = []
-messages = []  # Store chat messages
+messages = []  # Store chat history
 fruits = [
     "Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blueberry", "Cherry", "Clementine", "Coconut", 
     "Cranberry", "Currant", "Date", "Dragonfruit", "Elderberry", "Fig", "Grape", "Grapefruit", "Guava", 
@@ -40,18 +40,18 @@ def generate_username():
 def index():
     if request.method == 'POST':
         print('post request')
-        name = generate_username()
-        session['username'] = name
-        if name:
-            users.append(name)  # Add the name to the users list
-            return redirect(url_for('chat', name=name))
+        username = generate_username()
+        session['username'] = username
+        if username:
+            users.append(username)
+            return redirect(url_for('chat', name=username))
     return render_template('index.html')
 
 # Route for the public chat page
 @app.route('/chat/<name>')
 def chat(name):
     if name not in users:
-        return redirect(url_for('index'))  # Redirect if user is not in the list
+        return redirect(url_for('index'))
     return render_template('chat.html', name=name, messages=messages)
 
 # WebSocket event to handle messages
@@ -59,9 +59,9 @@ def chat(name):
 def handle_message(data):
     print(data['username'])
     if data['username'] == session['username']:
-        message_content = f"{data['username']}: {data['message']}"  # Format the message
-        messages.append(message_content)  # Store the formatted message
-        send(message_content, broadcast=True)  # Broadcast the formatted message
+        message_content = f"{data['username']}: {data['message']}"
+        messages.append(message_content)
+        send(message_content, broadcast=True)
     else:
         send({'redirect': 'https://www.youtube.com/watch?v=xvFZjo5PgG0'}, broadcast=False)
 
